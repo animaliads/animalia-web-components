95ba2d6f4d55ebe612b8274c0602156e8f0e65a7
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-04-08 10:10:22 -0300

    feat: tema dinamico

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { html } from 'lit-html';
+
 export const parameters = {
   viewMode: 'docs',
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -15,3 +17,32 @@ export const parameters = {
     },
   },
 };
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'Fluig',
+    toolbar: {
+      icon: 'circle',
+      // array of plain string values or MenuItem shape (see below)
+      items: [
+        { value: 'poui', title: 'PO UI' },
+        { value: 'fluig', title: 'Fluig' },
+      ],
+    },
+  },
+};
+
+const decorator = (story, context) => {
+  const theme = context.globals.theme;
+  const links = {
+    poui: 'https://unpkg.com/@po-ds/po-ui-brand@1.0.0-beta.1/theme.css',
+    fluig: '',
+  };
+  return html`
+    <link rel="stylesheet" href="${links[theme]}" />
+    ${story()}
+  `;
+};
+export const decorators = [decorator];
