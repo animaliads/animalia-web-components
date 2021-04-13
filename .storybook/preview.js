023863ae0bb0474d93b292d3c12db3fc756f33ec
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-04-13 10:28:26 -0300

    fix: correção versão dos tokens

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -36,8 +36,8 @@ export const globalTypes = {
 const decorator = (story, context) => {
   const theme = context.globals.theme;
   const links = {
-    poui: 'https://unpkg.com/@po-ds/po-ui-brand@1.0.0-beta.2/theme.css',
-    fluig: 'https://unpkg.com/@po-ds/fluig-brand@1.0.0-beta.2/theme.css',
+    poui: 'https://unpkg.com/@po-ds/po-ui-brand/theme.css',
+    fluig: 'https://unpkg.com/@po-ds/fluig-brand/theme.css',
   };
   return html`
     <link rel="stylesheet" href="${links[theme]}" />
