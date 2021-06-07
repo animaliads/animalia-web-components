9be4df241e3af5644f4f5104dbe1effeae1dfd0b
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-06-07 14:48:13 -0300

    feat(button): novo componente (pós piloto)

--- packages/button/src/button.style.ts
+++ packages/button/src/button.style.ts
@@ -1,49 +1,50 @@
-export const style = `
+import { css } from 'lit-element';
+
+export const style = css`
   button {
     /* Custom */
     font-family: var(--font-family);
-    color: var(--text-color);
     font-size: var(--font-size);
+    font-weight: var(--font-weight-bold);
     line-height: var(--line-height);
-
-    background-color: var(--background);
     border-radius: var(--border-radius);
     padding: var(--spacing-squish);
 
+    /* Fixed */
+    letter-spacing: var(--letter-spacing-auto);
+  }
+
+  :host([kind='primary']) button {
+    background-color: var(--background);
+    color: var(--text-color);
     box-shadow: var(--shadow);
 
-    /* Fixed */
-    font-weight: var(--font-weight-bold);
-    letter-spacing: var(--letter-spacing-auto);
     border: none;
   }
 
-  button:hover {
-    /* Custom */
+  :host([kind='primary']) button:hover {
     background-color: var(--background-hover);
+    color: var(--text-color-hover);
 
-    /* Fixed */
     cursor: pointer;
   }
 
-  button:focus {
-    /* Fixed */
-    /* outline-width in box-shadow */
-    outline: none;
-    box-shadow: 0 0 0 var(--border-width-lg) var(--outline-color-focused);
+  :host([kind='primary']) button:focus {
+    outline-color: var(--outline-color-focused);
+
+    outline-width: var(--border-width-lg);
   }
 
-  button:active {
-    /* Custom */
+  :host([kind='primary']) button:active {
     background-color: var(--background-pressed);
+    color: var(--text-color-pressed);
+    outline-color: var(--outline-color-focused);
 
-    /* Fixed */
-    /* outline-width in box-shadow */
-    box-shadow: 0 0 0 var(--border-width-lg) var(--outline-color-focused);
+    outline-width: var(--border-width-lg);
     cursor: pointer;
   }
 
-  button[aria-disabled='true'] {
+  :host([kind='primary'][disabled]) button[aria-disabled='true'] {
     /* Custom */
     background-color: var(--background-disabled);
     color: var(--text-color-disabled);
@@ -52,12 +53,111 @@ export const style = `
     cursor: not-allowed;
   }
 
-  button[aria-disabled='true']:focus {
+  :host([kind='primary'][disabled]) button[aria-disabled='true']:focus {
     /* Custom */
     outline-color: var(--outline-color-disabled);
 
     /* Fixed */
-    outline-offset: 2px;
     outline-width: var(--border-width-lg);
+    cursor: not-allowed;
+  }
+
+  :host([kind='secondary']) button {
+    color: var(--text-color);
+    border-color: var(--border-color);
+    border-width: var(--border-width);
+
+    background-color: transparent;
+    border-style: solid;
+    box-shadow: var(--shadow-none);
+  }
+
+  :host([kind='secondary']) button:hover {
+    color: var(--text-color-hover);
+    background-color: var(--background-hover);
+    border-color: var(--border-color-hover);
+
+    cursor: pointer;
+  }
+
+  :host([kind='secondary']) button:focus {
+    outline-color: var(--outline-color-focused);
+
+    outline-width: var(--border-width-lg);
+  }
+
+  :host([kind='secondary']) button:active {
+    color: var(--text-color-pressed);
+    background-color: var(--background-pressed);
+    border-color: var(--border-color-pressed);
+    outline-color: var(--outline-color-focused);
+
+    outline-width: var(--border-width-lg);
+    cursor: pointer;
+  }
+
+  :host([kind='secondary'][disabled]) button[aria-disabled='true'] {
+    /* Custom */
+    color: var(--text-color-disabled);
+    border-color: var(--border-color-disabled);
+
+    /* Fixed */
+    cursor: not-allowed;
+  }
+
+  :host([kind='secondary'][disabled]) button[aria-disabled='true']:focus {
+    /* Custom */
+    outline-color: var(--outline-color-disabled);
+
+    /* Fixed */
+    outline-width: var(--border-width-lg);
+    cursor: not-allowed;
+  }
+
+  :host([kind='tertiary']) button {
+    color: var(--text-color);
+
+    background-color: transparent;
+    border: none;
+    box-shadow: var(--shadow-none);
+  }
+
+  :host([kind='tertiary']) button:hover {
+    color: var(--text-color-hover);
+    background-color: var(--background-hover);
+
+    cursor: pointer;
+  }
+
+  :host([kind='tertiary']) button:focus {
+    outline-color: var(--outline-color-focused);
+
+    outline-width: var(--border-width-lg);
+  }
+
+  :host([kind='tertiary']) button:active {
+    color: var(--text-color-pressed);
+    background-color: var(--background-pressed);
+    outline-color: var(--outline-color-focused);
+
+    outline-width: var(--border-width-lg);
+    cursor: pointer;
+  }
+
+  :host([kind='tertiary'][disabled]) button[aria-disabled='true'] {
+    /* Custom */
+    color: var(--text-color-disabled);
+
+    /* Fixed */
+    cursor: not-allowed;
+  }
+
+  :host([kind='tertiary'][disabled]) button[aria-disabled='true']:focus {
+    /* Custom */
+    outline-color: var(--outline-color-disabled);
+
+    /* Fixed */
+    outline-width: var(--border-width-lg);
+    cursor: not-allowed;
   }
 `;
