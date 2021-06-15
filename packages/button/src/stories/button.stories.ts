93abcbd40a5ba8687c9e4b10eb7e96dea45f20b7
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-06-15 07:53:06 -0300

    feat(button): correção sample labs

--- packages/button/src/stories/button.stories.ts
+++ packages/button/src/stories/button.stories.ts
@@ -1,57 +1,6 @@
 import '../button';
 import { html } from 'lit-html';
 
-const customProperties = [
-  { name: '--font-family', description: 'nome da família de fonte' },
-  { name: '--text-color', description: 'cor do texto da label' },
-  { name: '--font-size', description: 'tamanho da fonte' },
-  { name: '--line-height', description: 'espaçamento entre linhas' },
-  { name: '--background', description: 'cor de background' },
-  { name: '--border-radius', description: 'arredondamento da borda' },
-  { name: '--spacing-squish', description: 'espaçamento interno (padding)' },
-  {
-    name: '--background-hover',
-    description: 'cor de background no evento hover',
-  },
-  {
-    name: '--outline-color-focused',
-    description: 'cor da borda externa durante o focus',
-  },
-  {
-    name: '--outline-color-pressed',
-    description: 'cor da borda externa durante o pressed',
-  },
-  {
-    name: '--outline-color-disabled',
-    description: 'cor da borda externa quando o botão estiver desabilitado',
-  },
-  {
-    name: '--background-pressed',
-    description: 'cor do background durante o pressed',
-  },
-  {
-    name: '--background-disabled',
-    description: 'cor do background quando o botão estiver desabilitado',
-  },
-  {
-    name: '--shadow',
-    description: 'propriedades de sombra',
-  },
-];
-
-const customPropertiesArgs = customProperties.reduce((acc, propertie) => {
-  const newValue = {
-    [propertie.name]: {
-      description: propertie.description,
-      table: {
-        category: 'Propriedades customizáveis (CSS)',
-      },
-    },
-  };
-
-  return { ...acc, ...newValue };
-}, {});
-
 export default {
   title: 'API/Button',
   component: 'ani-button',
@@ -113,7 +62,6 @@ export default {
         category: 'Eventos',
       },
     },
-    ...customPropertiesArgs,
   },
 };
 
