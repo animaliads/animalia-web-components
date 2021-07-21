2e5fb3194a1d432ad2399cdceb17a58994ca3a0e
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-07-21 15:53:19 -0300

    fix: correção background

--- packages/radio/src/radio.style.ts
+++ packages/radio/src/radio.style.ts
@@ -1,10 +1,44 @@
-export const radioStyle = `
+import { css } from 'lit-element';
+
+export const radioStyle = css`
   * {
     cursor: pointer;
   }
 
   :host {
     display: inline-block;
+
+    --width-md: 24px;
+    --width-lg: 32px;
+
+    --offset-checked: 6px;
+
+    --circle-width-md: calc(var(--width-md) - 12px);
+    --circle-width-lg: calc(var(--width-lg) - 12px);
+
+    --background-radial-md: radial-gradient(
+      var(--circle-width-md) circle at 10px 10px,
+      var(--color-selected) 50%,
+      transparent 57%
+    );
+
+    --background-radial-lg: radial-gradient(
+      var(--circle-width-lg) circle at 14px 14px,
+      var(--color-selected) 50%,
+      transparent 54.2%
+    );
+
+    --background-radial-md-disabled: radial-gradient(
+      var(--circle-width-md) circle at 10px 10px,
+      var(--color-disabled) 50%,
+      transparent 57%
+    );
+
+    --background-radial-lg-disabled: radial-gradient(
+      var(--circle-width-lg) circle at 14px 14px,
+      var(--color-disabled) 50%,
+      transparent 54.2%
+    );
   }
 
   input[type='radio'] {
@@ -20,17 +54,18 @@ export const radioStyle = `
   }
 
   input[size='medium'] {
-    width: 24px;
-    height: 24px;
+    width: var(--width-md);
+    height: var(--width-md);
   }
 
   input[size='large'] {
-    width: 32px;
-    height: 32px;
+    width: var(--width-lg);
+    height: var(--width-lg);
   }
 
   input[type='radio']:hover {
     box-shadow: 0 0 0 var(--border-width-lg) var(--shadow-color-hover);
+    border-color: var(--color-hover);
   }
 
   :host([disabled]),
@@ -44,35 +79,29 @@ export const radioStyle = `
   }
 
   input[type='radio']:checked {
-    background-color: var(--color-selected);
+    background: var(--background-radial-md);
+    background-color: var(--background);
     border-color: var(--color-selected);
-
     padding: 4px;
+  }
+
+  input[type='radio'][size='large']:checked {
+    background: var(--background-radial-lg);
+    background-color: var(--background);
   }
 
   input[type='radio'][aria-disabled='true'] {
     background-color: var(--background-disabled);
     border-color: var(--color-disabled);
-    padding: 0;
   }
 
   input[type='radio'][aria-disabled='true']:checked {
-    background: radial-gradient(
-      11px circle at 10px 10px,
-      var(--color-disabled) 50%,
-      transparent 54.2%
-    );
+    background: var(--background-radial-md-disabled);
     background-color: var(--background-disabled);
-    border-color: var(--color-disabled);
-    padding: 4px;
   }
 
   input[type='radio'][aria-disabled='true'][size='large']:checked {
-    background: radial-gradient(
-      19px circle at 14px 14px,
-      var(--color-disabled) 50%,
-      transparent 54.2%
-    );
+    background: var(--background-radial-lg-disabled);
     background-color: var(--background-disabled);
   }
 
