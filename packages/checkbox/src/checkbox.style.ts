bc54770884947b0ae83009fd5c64bbd2320d1cdf
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-07-27 18:00:32 -0300

    feat: ajuste cursor e mixed checked

--- packages/checkbox/src/checkbox.style.ts
+++ packages/checkbox/src/checkbox.style.ts
@@ -1,3 +1,74 @@
-export const checkboxStyle = `
+import { css } from 'lit-element';
 
+export const checkboxStyle = css`
+  :host {
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  .checkbox {
+    width: 24px;
+    height: 24px;
+    box-sizing: border-box;
+    display: inline-block;
+    background-color: var(--color-unchecked);
+    border-color: var(--border-color);
+
+    border-width: var(--border-width-md);
+    border-style: solid;
+    border-radius: var(--border-radius-md);
+
+    position: relative;
+  }
+
+  .checkbox:hover {
+    border-color: var(--color-hover);
+    box-shadow: 0 0 0 var(--border-width-lg) var(--shadow-color-hover);
+  }
+
+  .checkbox:focus-visible {
+    outline-color: var(--outline-color-focused);
+
+    outline-width: var(--border-width-lg);
+    outline-style: solid;
+    outline-offset: 2px;
+  }
+
+  :not([aria-checked='false']) .checkbox {
+    background-color: var(--color-checked);
+
+    border-color: transparent;
+  }
+
+  [aria-disabled='true'] .checkbox {
+    background-color: var(--color-unchecked-disabled);
+    border-color: var(--color-checked-disabled);
+
+    box-shadow: none;
+  }
+
+  :not([aria-checked='false'])[aria-disabled='true'] .checkbox {
+    background-color: var(--color-checked-disabled);
+  }
+
+  div[aria-disabled='true'] {
+    cursor: not-allowed;
+  }
+
+  [size='large'] .checkbox {
+    width: 32px;
+    height: 32px;
+  }
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  path,
+  rect {
+    fill: var(--color-unchecked);
+  }
 `;
--- packages/checkbox/src/checkbox.ts
+++ packages/checkbox/src/checkbox.ts
@@ -1,9 +1,19 @@
 import { CheckboxSize } from './enums/checkbox-size.enum';
+import { KeyCode } from './enums/key-code.enum';
 import { checkboxStyle } from './checkbox.style';
+
+const mixedIcon = `
+<svg width="12" height="3" viewBox="0 0 12 3" xmlns="http://www.w3.org/2000/svg">
+  <rect y="0.5" width="12" height="2"/>
+</svg>`;
+
+const checkIcon = `
+<svg width="12" height="10" viewBox="0 0 12 10" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0 5.32608L4.55114 9.5L12 2.10491L10.3864 0.5L4.55114 6.30151L1.60227 3.49433L0 5.32608Z"/>
+</svg>`;
 
 export default class Checkbox extends HTMLElement {
   shadow: ShadowRoot;
-
   checkboxElement: HTMLElement;
 
   get checked(): string {
@@ -18,6 +28,10 @@ export default class Checkbox extends HTMLElement {
   get disabled(): string {
     const disabled = this.getAttribute('disabled');
     return transformBooleanProperties(disabled);
+  }
+
+  get checkedIcon(): string {
+    return this.checked === 'mixed' ? mixedIcon : checkIcon;
   }
 
   get size(): string {
@@ -37,7 +51,10 @@ export default class Checkbox extends HTMLElement {
   connectedCallback(): void {
     this.render();
 
+    this.checkboxElement = this.shadow.querySelector('.checkbox');
+
     this.addEventListener('click', this.onClick);
+    this.addEventListener('keydown', this.handleKeyDown);
   }
 
   attributeChangedCallback(): void {
@@ -47,38 +64,43 @@ export default class Checkbox extends HTMLElement {
   render(): void {
     this.shadow.innerHTML = `
         <style>${checkboxStyle}</style>
-        <label>
-          <input
-            type="checkbox"
-            size="${this.size}"
-            aria-disabled="${this.disabled}"
-            ${this.checked === 'true' ? 'checked' : ''}
-          >
+        <div
+          size="${this.size}"
+          role="checkbox"
+          aria-checked="${this.checked}"
+          aria-disabled="${this.disabled}">
+          <span class="checkbox" tabindex="0">
+            ${this.checked === 'false' ? '' : this.checkedIcon}
+          </span>
           <slot></slot>
-        </label>
+        </div>
     `;
-    // this.shadow.innerHTML = `
-    //     <style>${checkboxStyle}</style>
-    //     <div role="checkbox"
-    //         class="group_checkbox"
-    //         aria-checked="false"
-    //         tabindex="0">
-    //           <svg width="12" height="10" viewBox="0 0 12 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-    //           <path d="M0 5.32608L4.55114 9.5L12 2.10491L10.3864 0.5L4.55114 6.30151L1.60227 3.49433L0 5.32608Z" fill="white"/>
-    //           </svg>
-
-    //         <slot></slot>
-    //     </div>
-    // `;
     this.setDefaultSize();
   }
 
   private onClick(event) {
     if (this.disabled === 'false') {
-      this.checked = this.checked === 'true' ? 'false' : 'true';
+      this.toggleCheckbox();
     } else {
       event.preventDefault();
     }
+  }
+
+  private handleKeyDown(event) {
+    const keyCode = event.keyCode;
+
+    if (keyCode === KeyCode.SPACE) {
+      if (this.disabled === 'false') {
+        this.toggleCheckbox();
+        this.checkboxElement.focus();
+      } else {
+        event.preventDefault();
+      }
+    }
+  }
+
+  private toggleCheckbox() {
+    this.checked = this.checked === 'true' ? 'false' : 'true';
   }
 
   private setDefaultSize() {
