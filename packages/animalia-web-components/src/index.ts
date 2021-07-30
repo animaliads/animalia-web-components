56f3799890096941be600c6e8ca4d98af15dbe94
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-07-30 15:13:35 -0300

    feat(textarea): estrutura inicial

--- packages/animalia-web-components/src/index.ts
+++ packages/animalia-web-components/src/index.ts
@@ -1,3 +1,4 @@
 export * from '../../button/src/index';
+export * from '../../textarea/src/index';
 export * from '../../link/src/index';
 export * from '../../radio/src/index';
---
+++ packages/textarea/src/field.ts
@@ -0,0 +1,9 @@
+export interface Field {
+  readonly placeholder: string;
+
+  readonly label: string;
+
+  readonly disabled: string;
+
+  readonly readonly: string;
+}
