193eb7c72d079c5d7ad28aa0e8e84756bf78b3cf
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-09-28 11:00:08 -0300

    feat(switch): novo componente (#25)
    Novo componente

--- packages/animalia-web-components/src/index.ts
+++ packages/animalia-web-components/src/index.ts
@@ -3,4 +3,5 @@ export * from '../../textarea/src/index';
 export * from '../../link/src/index';
 export * from '../../radio/src/index';
 export * from '../../checkbox/src/index';
+export * from '../../switch/src/index';
 export * from '../../textfield/src/index';
