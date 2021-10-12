c3ccb5b83d30f28521c54064cdb36ac2646baab9
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-10-12 15:14:55 -0300

    feat: correção para o build geral

---
+++ packages/index.ts
@@ -0,0 +1,7 @@
+export * from './button/src/index';
+export * from './checkbox/src/index';
+export * from './link/src/index';
+export * from './radio/src/index';
+export * from './switch/src/index';
+export * from './textarea/src/index';
+export * from './textfield/src/index';
--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,11 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 
 import path from 'path';
-import babel from '@rollup/plugin-babel';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { terser } from 'rollup-plugin-terser';
 
 import typescript from '@rollup/plugin-typescript';
+import typescript2 from 'rollup-plugin-typescript2';
 
 import copy from 'rollup-plugin-copy';
 
@@ -18,56 +18,66 @@ const name = LERNA_PACKAGE_NAME.split('/')[1].split('-')[1];
 
 const outputPathBase = `${LERNA_ROOT_PATH}/dist/${LERNA_PACKAGE_NAME}`;
 
-export default {
-  input: `${PACKAGE_ROOT_PATH}/src/index.ts`,
-  output: [
-    {
-      name,
-      file: `${LERNA_ROOT_PATH}/dist/@animaliads/web-components/index.js`,
-      format: 'umd',
-    },
-    {
-      name,
-      file: `${outputPathBase}/index.js`,
-      format: 'umd',
-      sourcemap: 'inline',
-    },
-  ],
-  plugins: [
-    copy({
-      targets: [
-        {
-          src: path.join(PACKAGE_ROOT_PATH, 'package.json'),
-          dest: outputPathBase,
-          transform: content => {
-            const packageJson = JSON.parse(content.toString());
-            delete packageJson.devDependencies;
-            delete packageJson.scripts;
-            delete packageJson.files;
-            packageJson.version = MAIN_VERSION;
-            packageJson.main = 'index.js';
+export default [
+  {
+    input: `${PACKAGE_ROOT_PATH}/src/index.ts`,
+    output: [
+      {
+        name,
+        file: `${outputPathBase}/index.js`,
+        format: 'umd',
+        sourcemap: true
+      },
+    ],
+    plugins: [
+      copy({
+        targets: [
+          {
+            src: path.join(PACKAGE_ROOT_PATH, 'package.json'),
+            dest: outputPathBase,
+            transform: content => {
+              const packageJson = JSON.parse(content.toString());
+              delete packageJson.devDependencies;
+              delete packageJson.scripts;
+              delete packageJson.files;
+              packageJson.version = MAIN_VERSION;
+              packageJson.main = 'index.js';
 
-            return Buffer.from(JSON.stringify(packageJson, null, 2), 'utf-8');
+              return Buffer.from(JSON.stringify(packageJson, null, 2), 'utf-8');
+            },
           },
-        },
-        {
-          src: path.join(PACKAGE_ROOT_PATH, 'README.md'),
-          dest: outputPathBase,
-        },
-      ],
-    }),
-    minifyHTML(), // reduzir html e css dentro de literals com javascript
-    typescript({
-      sourceMap: true,
-      declaration: true,
-      tsconfig: path.join(PACKAGE_ROOT_PATH, 'tsconfig.json'),
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: ['node_modules/**'],
-      extensions: ['.js', '.ts'],
-    }),
-    resolve(),
-    terser(), // minificar
-  ],
-};
+          {
+            src: path.join(PACKAGE_ROOT_PATH, 'README.md'),
+            dest: outputPathBase,
+          },
+        ],
+      }),
+      minifyHTML(),
+      typescript({
+        sourceMap: true,
+        declaration: true,
+        tsconfig: path.join(PACKAGE_ROOT_PATH, 'tsconfig.json'),
+      }),
+      resolve(),
+      terser(),
+    ],
+  },
+  {
+    input: `${LERNA_ROOT_PATH}/packages/index.ts`,
+    output: [
+      {
+        name,
+        file: `${LERNA_ROOT_PATH}/dist/@animaliads/web-components/index.js`,
+        format: 'umd',
+      },
+    ],
+    plugins: [
+      resolve(),
+      typescript2({
+        tsconfig: path.join(`${LERNA_ROOT_PATH}`, 'tsconfig.json'),
+      }),
+      minifyHTML(),
+      terser(),
+    ],
+  },
+];
