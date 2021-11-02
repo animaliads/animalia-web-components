bf5d39f87f2133fac69b3e7e959cc1da638b46a5
Gabrieli Barbosa <31509625+gabrielibarbosa@users.noreply.github.com>
2021-11-02 17:28:48 -0300

    fix: ajustes do build no windows (#34)
    * fix: ajustes do build no windows
    
    Corrige erro ao gerar o build, não estava gerando os arquivos REAME  e package.json, também não estava gerando os arquivos .d.ts

--- scripts/packages-rollup.config.js
+++ scripts/packages-rollup.config.js
@@ -4,18 +4,13 @@ import path from 'path';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { terser } from 'rollup-plugin-terser';
 
-import typescript from '@rollup/plugin-typescript';
-
-import copy from 'rollup-plugin-copy';
-
-const MAIN_VERSION = process.env.npm_package_version;
+import typescript2 from 'rollup-plugin-typescript2';
 
 const PACKAGE_ROOT_PATH = process.cwd();
 const { LERNA_PACKAGE_NAME, LERNA_ROOT_PATH } = process.env;
 
 const name = LERNA_PACKAGE_NAME.split('/')[1].split('-')[1];
-
-const outputPathBase = `${LERNA_ROOT_PATH}/dist/${LERNA_PACKAGE_NAME}`;
+const outputPathBase = path.join(LERNA_ROOT_PATH, 'dist', LERNA_PACKAGE_NAME);
 
 export default {
   input: `${PACKAGE_ROOT_PATH}/src/index.ts`,
@@ -28,30 +23,8 @@ export default {
     },
   ],
   plugins: [
-    copy({
-      targets: [
-        {
-          src: path.join(PACKAGE_ROOT_PATH, 'package.json'),
-          dest: outputPathBase,
-          transform: content => {
-            const packageJson = JSON.parse(content.toString());
-            delete packageJson.devDependencies;
-            delete packageJson.scripts;
-            delete packageJson.files;
-            packageJson.version = MAIN_VERSION;
-            packageJson.main = 'index.js';
-
-            return Buffer.from(JSON.stringify(packageJson, null, 2), 'utf-8');
-          },
-        },
-        {
-          src: path.join(PACKAGE_ROOT_PATH, 'README.md'),
-          dest: outputPathBase,
-        },
-      ],
-    }),
     minifyHTML(),
-    typescript({
+    typescript2({
       sourceMap: true,
       declaration: true,
       tsconfig: path.join(PACKAGE_ROOT_PATH, 'tsconfig.json'),
