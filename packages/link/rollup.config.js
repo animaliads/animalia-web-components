38e37677ea1557c7cb658f1ed3387ba7d1b93f63
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-11-08 16:54:08 -0300

    feat: wip

---
+++ packages/link/rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+const input = 'src/index.ts';
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
+const plugins = [typescript({ typescript: require('typescript') })];
+
+export default [
+  {
+    input,
+    output: { name: 'link', file: pkg.module, format: 'esm', sourcemap: true },
+    plugins,
+    external,
+  },
+];
---
+++ packages/textfield/rollup.config.js
@@ -0,0 +1,25 @@
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+const input = 'src/index.ts';
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
+const plugins = [typescript({ typescript: require('typescript') })];
+
+export default [
+  {
+    input,
+    output: {
+      name: 'textfield',
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins,
+    external,
+  },
+];
