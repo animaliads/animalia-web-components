4da011f18b5981563b70080f4825d282a2c6883b
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-11-19 11:54:14 -0300

    docs: rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
+/**
+ * Função genérica para geração dos pacotes de distribuição dos pacotes.
+ * Esta função é baseada na configuração padrão do Rollup para geração de bundle.
+ *
+ * @param {string} input caminho do arquivo index.ts
+ * @param {*} pkg conteúdo do package.json
+ * @returns Rollup config
+ */
 export function getRollupConfig(input, pkg) {
   const plugins = [typescript({ typescript: require('typescript') }), terser()];
 
