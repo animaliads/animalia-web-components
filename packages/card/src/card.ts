410d3c7cbb65547c4d4411a3142b6cc8c3462b75
gabrielibarbosa <ifc.gabrieli.barbosa@gmail.com>
2021-11-26 09:18:35 -0300

    feat(card): adiciona propriedades interativas

--- packages/card/src/card.ts
+++ packages/card/src/card.ts
@@ -2,6 +2,28 @@ import { cardStyle } from './style';
 
 export default class Card extends HTMLElement {
   shadow: ShadowRoot;
+  cardElement: HTMLDivElement;
+
+  get type(): string {
+    return this.getAttribute('type');
+  }
+
+  get href(): string {
+    return this.getAttribute('href');
+  }
+
+  get selected(): string {
+    const selected = this.getAttribute('selected');
+    return transformBooleanProperties(selected);
+  }
+
+  set selected(value: string) {
+    this.setAttribute('selected', value);
+  }
+
+  static get observedAttributes(): Array<string> {
+    return ['type', 'href', 'selected'];
+  }
 
   constructor() {
     super();
@@ -10,6 +32,16 @@ export default class Card extends HTMLElement {
 
   connectedCallback(): void {
     this.render();
+
+    this.addEventListener('click', this.onClick);
+    this.addEventListener('keydown', this.handleKeyDown);
+
+    this.updateAttributes();
+  }
+
+  disconnectedCallback(): void {
+    this.removeEventListener('click', this.onClick);
+    this.removeEventListener('keydown', this.handleKeyDown);
   }
 
   render(): void {
@@ -20,6 +52,64 @@ export default class Card extends HTMLElement {
         </div>
     `;
   }
+
+  private onClick() {
+    if (this.type === 'selectable') {
+      this.toggleSelected();
+    }
+  }
+
+  private handleKeyDown(event) {
+    if (this.type === 'selectable' && event.keyCode === 32) {
+      this.toggleSelected();
+    }
+  }
+
+  private toggleSelected() {
+    this.selected = this.selected === 'true' ? 'false' : 'true';
+  }
+
+  attributeChangedCallback(): void {
+    this.updateAttributes();
+  }
+
+  private updateAttributes() {
+    if (this.type === 'link') {
+      this.shadow.innerHTML = `
+        <style>${cardStyle}</style>
+        <a class="ani-card" href="${this.href}">
+          <slot></slot>
+        </a>
+     `;
+    }
+    if (this.type === 'selectable') {
+      if (this.selected === 'true') {
+        this.shadow.innerHTML = `
+          <style>${cardStyle}</style>
+          <div class="ani-card ani-card-selectable" tabindex="0">
+            <slot></slot>
+          </div>
+        `;
+        return;
+      }
+      this.shadow.innerHTML = `
+      <style>${cardStyle}</style>
+      <div class="ani-card" tabindex="0">
+        <slot></slot>
+      </div>
+    `;
+    }
+  }
 }
 
 customElements.define('ani-card', Card);
+
+function transformBooleanProperties(value: string) {
+  if (value === '') {
+    return 'true';
+  } else if (value === null) {
+    return 'false';
+  }
+
+  return value;
+}
--- packages/card/src/style.ts
+++ packages/card/src/style.ts
@@ -1,4 +1,12 @@
 export const cardStyle = `
+  :host() {
+    display: inline-block;
+  }
+
+  a {
+    display: inline-block;
+  }
+
   .ani-card {
     border-radius: var(--border-radius);
     border-color: var(--border-color);
@@ -7,5 +15,30 @@ export const cardStyle = `
     padding: var(--padding);
 
     border-style: solid;
+    text-decoration: none;
+    color: inherit;
+    box-sizing: border-box;
+    width: 100%;
+  }
+
+  .ani-card:hover {
+    border-color: var(--border-color-hover);
+    box-shadow: var(--shadow-hover);
+
+    cursor: pointer;
+  }
+
+  .ani-card:focus {
+    border-color: var(--color-focused);
+    outline-color: var(--outline-color-focused);
+
+    outline-width: var(--border-width-lg);
+    outline-style: solid;
+    outline-offset: 2px;
+  }
+
+  .ani-card-selectable {
+    border-color: var(--border-color-selected);
+    background-color: var(--background-selected)
   }
 `;
