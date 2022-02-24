6a0efe4697f0efbe4beb0be5278e8e2dd8e5f595
Gabrieli Barbosa <31509625+gabrielibarbosa@users.noreply.github.com>
2022-02-24 16:39:02 -0300

    feat(icone): novo componente (#46)
    Co-authored-by: Nicole Oliveira <nicoleoliveira1995@gmail.com>

---
+++ packages/icon/prepare.mjs
@@ -0,0 +1,40 @@
+import { readdir } from 'fs/promises';
+import { readFileSync, writeFile } from 'fs';
+import path from 'path';
+
+const iconsDir = 'svg';
+const finalFilePath = path.join('src', 'svg.ts');
+
+try {
+  const files = await readdir(iconsDir);
+
+  const svgs = getSvgs(files);
+  writeSvgInFile(svgs);
+} catch (err) {
+  console.error(err);
+}
+
+function writeSvgInFile(svgs) {
+  const content = `// prettier-ignore
+/* eslint-disable */
+export const svgs = ${JSON.stringify(svgs, null, 4)}
+`;
+
+  writeFile(path.join(finalFilePath), content, err => {
+    if (err) console.log(err);
+  });
+}
+
+function getSvgs(files) {
+  const svgs = {};
+
+  for (const filename of files) {
+    const file = readFileSync(path.join(iconsDir, filename), {
+      encoding: 'utf8',
+    });
+
+    svgs[filename.replace('.svg', '')] = file;
+  }
+
+  return svgs;
+}
