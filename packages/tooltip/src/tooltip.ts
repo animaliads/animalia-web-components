f728020a5b7d92df77a5af0c7bc7de1eb504f0c7
Lucy <lucyjbrandao@gmail.com>
2023-04-26 16:35:26 -0300

    feat: continuação desenvolvimento tooltip

--- packages/tooltip/src/tooltip.ts
+++ packages/tooltip/src/tooltip.ts
@@ -4,29 +4,48 @@ import { tooltipStyle } from './tooltip.style';
 @Component('ani-tooltip')
 export class Tooltip extends HTMLElement {
   private tooltip: HTMLDivElement;
+  private triggerElement: HTMLElement;
 
   constructor() {
     super();
 
-    this.tooltip = document.createElement('div');
-    this.tooltip.classList.add('tooltip');
-    this.tooltip.setAttribute('role', 'tooltip');
-
     const shadow = this.attachShadow({ mode: 'open' });
-    shadow.appendChild(this.tooltip);
 
     this.addEventListener('mouseenter', this.showTooltip.bind(this));
     this.addEventListener('mouseleave', this.hideTooltip.bind(this));
 
-    shadow.innerHTML += `
+    shadow.innerHTML = `
       <style>
         ${tooltipStyle}
       </style>
       <slot></slot>
-      <div class="tooltip">
+      <div class="tooltip" role="tooltip" aria-hidden="true">
         <slot name="content"></slot>
       </div>
     `;
+
+    this.tooltip = shadow.querySelector('.tooltip') as HTMLDivElement;
+  }
+
+  connectedCallback(): void {
+    const assignedNodes = (
+      this.shadowRoot.querySelector('slot') as HTMLSlotElement
+    ).assignedNodes();
+    this.triggerElement = assignedNodes.find(
+      node => node instanceof HTMLElement
+    ) as HTMLElement;
+
+    if (this.triggerElement) {
+      const tooltipId = `tooltip-${Math.random().toString(36).substr(2, 10)}`;
+      this.tooltip.id = tooltipId;
+      this.triggerElement.setAttribute('aria-describedby', tooltipId);
+      this.triggerElement.setAttribute('tabindex', '0');
+    }
+  }
+
+  get position(): string {
+    const position = this.getAttribute('position');
+    return position || 'top';
   }
 
   private showTooltip() {
@@ -36,10 +55,12 @@ export class Tooltip extends HTMLElement {
 
     this.tooltip.innerText = content;
     this.tooltip.style.opacity = '1';
+    this.tooltip.setAttribute('aria-hidden', 'false');
   }
 
   private hideTooltip() {
     this.tooltip.style.opacity = '0';
+    this.tooltip.setAttribute('aria-hidden', 'true');
   }
 
   private truncateText(text: string, limit: number) {
